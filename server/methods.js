29fb9408d994ea152922a0a87b13f58a18a0f921
Chris LaVoy <clavoy@asu.edu>
2016-01-22 22:56:46 -0700

    Added Underscore, geolocation, and http packages; started implementing call to implement pulling locations

---
+++ server/methods.js
@@ -0,0 +1,49 @@
+var callCount = 1000;
+var key = 'AIzaSyBi-7Mphqp8TjX5KhYE-r-rGRYByt_3qZk';
+var minPrice = 0;
+var maxPrice = 4;
+var response = "";
+
+Meteor.methods({
+  pullPlaces: function(location, token){
+    if (token !== null) {
+      response = HTTP.call("GET", "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+        {params:
+          {
+            key: key,
+            pagetoken: token
+          }
+        }
+      );
+    } else {
+      response = HTTP.call("GET", "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
+        {params:
+          {
+            key: key,
+            location: location.lat+","+location.lng,
+            rankby: 'distance',
+            minPrice: minPrice,
+            maxPrice: maxPrice,
+            types: "food|restaurant|point_of_interest|meal_takeaway|establishment",
+            keyword: "fast food"
+          }
+        }
+      );
+    }
+    
+    token = response.data.next_page_token;
+    var data = response.data.results;
+
+    for (var i = 0; i < data.length; i++) {
+      _.extend(data[i], {loc: {type: "Point", coordinates: [data[i].geometry.location.lng, data[i].geometry.location.lat]}});
+      _.extend(data[i], {lastAccessed: Date.now()});
+      Places.upsert({place_id: data[i].place_id}, {$set: data[i]});
+    }
+
+    if (token !== undefined && token !== null) {
+      Meteor.setTimeout(function(){
+        Meteor.call("pullPlaces", location, token);
+      }, 2000);
+    }
+  }
+});
