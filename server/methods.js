9827ebac6dda9a9949004bfd15c27939d739269c
Chris LaVoy <clavoy@asu.edu>
2016-01-22 23:05:02 -0700

    added untracked folder

--- server/methods.js
+++ server/methods.js
@@ -30,10 +30,8 @@ Meteor.methods({
         }
       );
     }
-    
     token = response.data.next_page_token;
     var data = response.data.results;
-
     for (var i = 0; i < data.length; i++) {
       _.extend(data[i], {loc: {type: "Point", coordinates: [data[i].geometry.location.lng, data[i].geometry.location.lat]}});
       _.extend(data[i], {lastAccessed: Date.now()});
@@ -45,5 +43,16 @@ Meteor.methods({
         Meteor.call("pullPlaces", location, token);
       }, 2000);
     }
+  },
+  measureDistance: function(lat1, lon1, lat2, lon2){
+    // TODO: Make this work
+    // this.unblock();
+    // Formula found at: http://www.movable-type.co.uk/scripts/latlong.html
+    // gives distance in meters
+    return Math.acos( Math.sin(lat1 * (Math.PI / 180))*Math.sin(lat2 *
+      (Math.PI / 180)) + Math.cos(lat1 * (Math.PI / 180))*Math.cos(lat2 *
+        (Math.PI / 180)) * Math.cos((lon2-lon1) * (Math.PI / 180)) ) * 6371000;
+    // console.log(d / 1000);
+    // return d;
   }
 });
